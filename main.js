804562bf78507f78a432a4a0e4a253a81b2aa3fc
Meghan Debity <mdebity@gmail.com>
2018-04-06 13:45:01 -0500

    trying new approach to object structure in document

--- main.js
+++ main.js
@@ -1,49 +1,17 @@
 //Write all objects, properties, and values for campaign
-const campaignTrack = Object.create({}, {
-    district: {
-        enumerable: true,
-        writable: false,
-        value: "Hogwarts" 
-    },
-    platformStatements: {
-        enumerable: true,
-        writable: false, 
-        value: ["For Taxes", "Job Creation", "Infrastructure Needs to be Overhauled", "Health Care is for Everyone", "Against Crime", "Wants 'For the People' Enforcement"]
-    },
-    campaignURL: {
-        enumerable: true,
-        writable: false,
-        value: "donate.to.me.com"
-    },
-    calendar: {
-        enumerable: true,
-        writable: false,
-        value: ""
-    },
-    volenteerInfo: {
-        enumerable: true,
-        writable: false,
-        //values--consider document.getElementByID like in flash exercise
-        value: // Volunteer information Name Address Email Phone number Availability What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
-    },
-    biography: {
-        enumerable: true,
-        writable: false,
-        value: "Elizabeth is actually Dumbledore."
-    },
-    imageGallery: {
-        enumerable: true,
-        writable: false,
-        value: //pull from CSS? headshot, family, constituents
-    },
-    missionStatement: {
-        enumerable: true,
-        writable: false,
-        value: "To conquer the world...I mean help people."
-    },
-    url-vote: {
-        enumerable: true,
-        writable: false,
-        value: ""
-    }
+//variables with inherent properties that protect similar to writable
+const candidate = Object.create({}, {
+    enumerable: true,
+    writable: false,
+    district: "Hogwarts",
+    biography: "Quite the character! He has changed his name at least once and it can't be said. Fun fact: our Lord has kinda died before.",
+    mission_statement: "Wants to live forever and rule."
+})
+
+const images = Object.create({}, {
+    enumerable: true,
+    writable: false,
+    headshot: "headshot.jpg",
+    family_portrait: "family.jpg",
+    constituents: "peopleHeRules.jpg"
 })
