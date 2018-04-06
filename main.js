eaef0f3eac7ca0aac8770c3bf6d8247f4aebf774
Meghan Debity <mdebity@gmail.com>
2018-04-06 11:24:41 -0500

    first lines of JS

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //Write all objects, properties, and values for campaign
+const campaignTrack = Object.create({}, {
+    district: {
+        enumerable: true,
+        writable: false,
+        value: "Hogwarts" 
+    },
+    platformStatements: {
+        enumerable: true,
+        writable: false, 
+        value: ["For Taxes", "Job Creation", "Infrastructure Needs to be Overhauled", "Health Care is for Everyone", "Against Crime", "Wants 'For the People' Enforcement"]
+    }
+})
