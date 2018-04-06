88bd7b7e257e301d7a619e8e4f9c64dda529d5c7
Meghan Debity <mdebity@gmail.com>
2018-04-06 13:45:46 -0500

    trying new structure for objects in file

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ const images = Object.create({}, {
     family_portrait: "family.jpg",
     constituents: "peopleHeRules.jpg"
 })
+const statements = Object.create({}, {
+    enumerable: true,
+    writable: false,
+    taxes: "Taxes don't interest me, only power and fear.",
+    jobs: "Do your job as my Death Eaters see fit or you will be terminated.",
+    infrastructure: "Infrastructure should reflect my cold and hard policies.",
+    health_care: "The health of all witches and wizards is an absolute focus. All mudbloods will be exterminated for the betterment of all.",
+    crime_enforcement: "Greyback will be the head of my policy force and his enforcement will be at his discretion."
+})
+ const links = Object.create({}, {
+     enumerable: true,
+     writable: false,
+     donate_link: "#",
+     vote_link: "##"
+ })
+
+ const volunteers = Object.create({}, {
+     enumerable: true,
+     writable: false,
+     title: "Death Eater",
+     first_name: ,
+     last_name: ,
+     address: ,
+     email: ,
+     phone_number: ,
+     availability: ,
+     activities: ,
+ })
